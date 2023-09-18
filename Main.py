9a2ba14b415b4f50de97e412ba7e5c6927e00a78
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2023-09-18 18:20:04 +0200

    Save

--- Main.py
+++ Main.py
@@ -1,14 +1,16 @@
 import tkinter as tk
 from tkinter import filedialog
-import csv
 from tkinter import ttk
+import csv
 import re
-
-listeTest = ['4', '3', 'Mon May 11 03:17:40 UTC 2009', 'kindle2', 'tpryan', '@stellargirl I loooooooovvvvvveee my Kindle2. Not that the DX is cool, but the 2 is fantastic in its own right.']
 
 liste_apprentissage = []
 liste_test = []
-algorithmes = [{"name" : "Dictionnaire", "description" : "blabla sur dictionnaire"}, {"name" : "KNN", "description" : "blaba de KNN"}, {"name" : "Bayes", "description" : "blabla sur Bayes"}]
+algorithmes = [
+    {"name": "Dictionnaire", "description": "blabla sur dictionnaire"},
+    {"name": "KNN", "description": "blabla de KNN"},
+    {"name": "Bayes", "description": "blabla sur Bayes"}
+]
 
 def quit():
     main_window.destroy()
@@ -21,20 +23,26 @@ def convert_csv_to_list(liste_a_modifier):
             for line in csv_reader:
                 liste_a_modifier.append(line)
     if liste_a_modifier != []:
-        bouton_analyser.config(state="normal")
-        afficher_tweets_importes()  # Affiche les tweets importés après importation
+        bouton_analyser_tweets.config(state="normal")
+        bouton_sauvegarder.config(state="normal")  # Activer le bouton "Sauvegarder"
+        afficher_tweets_importes()
+
+def sauvegarder_textes_nettoyes():
+    # Récupérer les textes nettoyés de la liste d'apprentissage
+    textes_nettoyes = [nettoyage(ligne[5]) for ligne in liste_apprentissage]
+    
+    # Écrire les textes nettoyés dans un fichier texte
+    with open("textes_nettoyes.txt", "w", encoding="utf-8") as fichier:
+        for texte in textes_nettoyes:
+            fichier.write(texte + "\n")
 
 def analyse():
     pass
 
 def nettoyage(commentaire):
-    # #1, Replace @username with @
     commentaire_nettoye = re.sub(r'(@[a-zA-Z0-9]+)', '@', commentaire)
-    # #2, Add white space character before and after ! ? : . " ; ,
     commentaire_nettoye = re.sub(r'([!?":;,])', r' \1 ', commentaire_nettoye)
-    # #3, Replace dollar values with variable ( $14.99 => $XX) (idem euros)
     commentaire_nettoye = re.sub(r'(\$ ?\d+\.\d+)', '$XX', commentaire_nettoye)
-    # #4, Replace percentage values with variable (25n% => XXn%)
     commentaire_nettoye = re.sub(r'([0-9]{1,2}\%)', 'XX%', commentaire_nettoye)
     return commentaire_nettoye
 
@@ -45,18 +53,18 @@ def on_selection(event):
             description_algo.config(text=algo["description"])
 
 def afficher_tweets_importes():
-    text_area.delete(1.0, tk.END)  # Efface le contenu précédent du Text widget
+    text_area.delete(1.0, tk.END)
     for ligne in liste_test:
-        commentaire = ligne[5]  # Supposons que le commentaire est dans la sixième colonne.
+        commentaire = ligne[5]
         commentaire_nettoye = nettoyage(commentaire)
-        text_area.insert(tk.END, commentaire_nettoye + "\n")  # Ajoute le commentaire nettoyé avec un saut de ligne
+        text_area.insert(tk.END, commentaire_nettoye + "\n")
 
 def analyser_tweets():
-    # Mettez ici le code pour analyser les tweets si nécessaire
-    pass
-def update_evaluation(*args):
-    new_value = combo_var.get()
-    liste_test[0] = new_value
+    for ligne in liste_test:
+        classification = classification_var.get()
+        algorithme = selection_algo.get()
+        # Insérez votre logique d'analyse ici et affichez le résultat dans le Text widget
+        pass
 
 main_window = tk.Tk()
 main_window.title("Interface Graphique")
@@ -66,45 +74,39 @@ selection_algo.pack(pady=10)
 selection_algo.set(algorithmes[0]["name"])
 
 description_algo = tk.Label(main_window, text=algorithmes[0]["description"])
-# description_algo.pack(padx=20, pady=20)
+description_algo.pack(padx=20, pady=20)
 
-bouton_import = tk.Button(main_window, text="Import fichier d'apprentissage", command=lambda: convert_csv_to_list(liste_apprentissage))
-bouton_import.pack()
+bouton_import_apprentissage = tk.Button(main_window, text="Importer fichier d'apprentissage", command=lambda: convert_csv_to_list(liste_apprentissage))
+bouton_import_apprentissage.pack()
 
-bouton_import = tk.Button(main_window, text="Import fichier de test", command=lambda: convert_csv_to_list(liste_test))
-bouton_import.pack(padx=20, pady=20)
+# Créez un espace visuel entre les boutons
+espace_entre_boutons = tk.Label(main_window, text="", height=1)
+espace_entre_boutons.pack()
 
-# Créez un widget Text pour afficher les tweets
+# Bouton "Sauvegarder" pour sauvegarder les textes nettoyés
+bouton_sauvegarder = tk.Button(main_window, text="Sauvegarder", state="disabled", command=sauvegarder_textes_nettoyes)
+bouton_sauvegarder.pack()
+
+bouton_import_test = tk.Button(main_window, text="Importer fichier de test", command=lambda: convert_csv_to_list(liste_test))
+bouton_import_test.pack(padx=20, pady=20)
+
 text_area = tk.Text(main_window, wrap=tk.WORD, width=40, height=10)
 text_area.pack(padx=20, pady=20)
 
-# Bouton pour analyser les tweets
+classification_var = tk.StringVar()
+classification_var.set("-1")
+
+classification_combo = ttk.Combobox(main_window, textvariable=classification_var, values=["-1", "0", "2", "4"])
+classification_combo.pack(padx=10, pady=10)
+
 bouton_analyser_tweets = tk.Button(main_window, text="Analyser les tweets", command=analyser_tweets, state="disabled")
 bouton_analyser_tweets.pack()
 
-# Espace entre les deux boutons
 espace_entre_boutons = tk.Label(main_window, text="", height=1)
 espace_entre_boutons.pack()
-label = tk.Label(main_window, text=listeTest[5])
-label.pack(padx=10, pady=10)
-# Créer une liste déroulante pour modifier le premier élément
-combo_var = tk.StringVar()
-combo_var.set(listeTest[0])  # Initialiser la liste déroulante avec la première valeur de la liste
-combo = ttk.Combobox(main_window, textvariable=combo_var, values=["-1", "0", "1", "2", "4"])
-combo.pack(padx=10, pady=10)
-
-# Associer la fonction de mise à jour à la liste déroulante
-combo_var.trace_add("write", update_evaluation)
-
-# Bouton pour afficher le résultat
-bouton_afficher_resultat = tk.Button(main_window, text="Afficher le résultat", command=afficher_tweets_importes, state="disabled")
-bouton_afficher_resultat.pack()
 
 resultat = tk.Label(main_window, text="Résultat : RIEN POUR L'INSTANT")
 resultat.pack(padx=20, pady=20)
-
-bouton_analyser = tk.Button(main_window, text="Sauvegarder")
-bouton_analyser.pack(padx=20, pady=20)  
 
 selection_algo.bind("<<ComboboxSelected>>", on_selection)
 
