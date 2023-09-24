5b4541ca05539b412f1740ef59112e68a4a21d00
CornetDeGlace <boudicrie@gmail.com>
2023-09-24 16:55:36 +0200

    edition tweet

--- Main.py
+++ Main.py
@@ -7,6 +7,7 @@ import Dictionnaire
 import KNN
 import Bayes
 
+elements_tests = [["blabla1", 1], ["blabla2", 2], ["blabla3", 3], ["blabla4", 4]]
 
 liste_apprentissage = []
 liste_test = []
@@ -30,6 +31,8 @@ def convert_csv_to_list(liste_a_modifier):
         bouton_analyser_tweets.config(state="normal")
         bouton_sauvegarder.config(state="normal")  # Activer le bouton "Sauvegarder"
         afficher_tweets_importes()
+
+    print(liste_test)
 
 def sauvegarder_textes_nettoyes():
     # Récupérer les textes nettoyés de la liste d'apprentissage
@@ -58,11 +61,13 @@ def on_selection(event):
             description_algo.config(text=algo["description"])
 
 def afficher_tweets_importes():
-    text_area.delete(1.0, tk.END)
-    for ligne in liste_test:
-        commentaire = ligne[5]
-        commentaire_nettoye = nettoyage(commentaire)
-        text_area.insert(tk.END, commentaire_nettoye + "\n")
+    for item in liste_test:
+        listbox.insert(tk.END, f"{item[5]} : {item[0]}")
+    # text_area.delete(1.0, tk.END)
+    # for ligne in liste_test:
+    #     commentaire = ligne[5]
+    #     commentaire_nettoye = nettoyage(commentaire)
+    #     text_area.insert(tk.END, commentaire_nettoye + "\n")
 
 def analyser_tweets():
     # Récupérer le nom de l'algorithme sélectionné
@@ -84,25 +89,40 @@ def analyser_tweets():
         # Gérer une sélection invalide (facultatif)
         print("Algorithme non pris en charge")
 
-def update_evaluation(*args):
-    new_value = combo_var.get()
-    liste_test[0] = new_value
-    for ligne in liste_test:
-        classification = classification_var.get()
-        algorithme = selection_algo.get()
-        # Insérez votre logique d'analyse ici et affichez le résultat dans le Text widget
-        pass
+# def update_evaluation(*args):
+#     new_value = combo_var.get()
+#     liste_test[0] = new_value
+#     for ligne in liste_test:
+#         classification = classification_var.get()
+#         algorithme = selection_algo.get()
+#         # Insérez votre logique d'analyse ici et affichez le résultat dans le Text widget
+#         pass
+
+def edit_item():
+    selected_index = listbox.curselection()
+    if selected_index:
+        new_value = combobox.get()
+        if new_value:
+            new_value = int(new_value)
+            index = selected_index[0]
+            liste_test[index][0] = new_value
+            listbox.delete(index)
+            listbox.insert(index, f"{liste_test[index][5]}, {new_value}")
+            print(liste_test)
 
 main_window = tk.Tk()
 main_window.title("Interface Graphique")
 
+# Liste algorithmes
 selection_algo = ttk.Combobox(main_window, values=[algo["name"] for algo in algorithmes], state="readonly")
 selection_algo.pack(pady=10)
 selection_algo.set(algorithmes[0]["name"])
 
+# Description de l'algorithme choisi
 description_algo = tk.Label(main_window, text=algorithmes[0]["description"])
 description_algo.pack(padx=20, pady=20)
 
+# Bouton d'import du fichier d'apprentissage
 bouton_import_apprentissage = tk.Button(main_window, text="Importer fichier d'apprentissage", command=lambda: convert_csv_to_list(liste_apprentissage))
 bouton_import_apprentissage.pack()
 
@@ -114,27 +134,69 @@ espace_entre_boutons.pack()
 bouton_sauvegarder = tk.Button(main_window, text="Sauvegarder", state="disabled", command=sauvegarder_textes_nettoyes)
 bouton_sauvegarder.pack()
 
+# Bouton d'import du fichier test
 bouton_import_test = tk.Button(main_window, text="Importer fichier de test", command=lambda: convert_csv_to_list(liste_test))
 bouton_import_test.pack(padx=20, pady=20)
 
-text_area = tk.Text(main_window, wrap=tk.WORD, width=40, height=10)
-text_area.pack(padx=20, pady=20)
 
-classification_var = tk.StringVar()
-classification_var.set("-1")
+# Zone de texte
+# text_area = tk.Text(main_window, wrap=tk.WORD, width=40, height=10)
+# text_area.pack(padx=20, pady=20)
 
-classification_combo = ttk.Combobox(main_window, textvariable=classification_var, values=["-1", "0", "2", "4"])
-classification_combo.pack(padx=10, pady=10)
+# classification_var = tk.StringVar()
+# classification_var.set("-1")
 
+# classification_combo = ttk.Combobox(main_window, textvariable=classification_var, values=["-1", "0", "2", "4"])
+# classification_combo.pack(padx=10, pady=10)
+
+# Bouton d'analyser des tweets
 bouton_analyser_tweets = tk.Button(main_window, text="Analyser les tweets", command=analyser_tweets, state="disabled")
 bouton_analyser_tweets.pack()
 
 espace_entre_boutons = tk.Label(main_window, text="", height=1)
 espace_entre_boutons.pack()
 
+# Zone de résultat
 resultat = tk.Label(main_window, text="Résultat : RIEN POUR L'INSTANT")
 resultat.pack(padx=20, pady=20)
 
 selection_algo.bind("<<ComboboxSelected>>", on_selection)
+
+# Zone de texte test
+scrolling_area = tk.Canvas(main_window)
+scrolling_area.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+scrolling_frame = tk.Frame(scrolling_area)
+
+scrolling_area.create_window((0, 0), window=scrolling_frame, anchor=tk.NW)
+scrollbar = tk.Scrollbar(main_window, command=scrolling_area.yview)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+scrolling_area.config(yscrollcommand=scrollbar.set)
+def on_canvas_configure(event):
+    scrolling_area.configure(scrollregion=scrolling_area.bbox("all"))
+
+scrolling_area.bind("<Configure>", on_canvas_configure)
+# for i in range(20):
+#     button = tk.Button(scrolling_frame, text=f"Bouton {i + 1}")
+#     button.pack()
+
+# Créez une Listbox pour afficher les deux éléments de chaque sous-liste
+listbox = tk.Listbox(scrolling_area)
+listbox.pack()
+
+# Ajoutez les éléments de la liste initiale à la Listbox
+for item in liste_test:
+    listbox.insert(tk.END, f"{item[5]} : {item[0]}")
+
+# Créez une liste déroulante (combobox) pour les choix possibles
+choices = [-1, 0, 1, 2, 3, 4]
+combobox = ttk.Combobox(scrolling_area, values=choices)
+combobox.pack()
+
+# Par défaut, sélectionnez le premier élément de la liste déroulante
+combobox.set(choices[0])
+
+# Créez un bouton pour éditer des éléments
+edit_button = tk.Button(scrolling_area, text="Éditer", command=edit_item)
+edit_button.pack()
 
 main_window.mainloop()
