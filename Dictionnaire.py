7222ddf30022b8ac9028209c70826ac0741f3a1d
CornetDeGlace <boudicrie@gmail.com>
2023-09-25 15:38:51 +0200

    fin algo dictionnaire

--- Dictionnaire.py
+++ Dictionnaire.py
@@ -4,27 +4,27 @@ class Dictionnaire:
         self.negative_list = []
         self.negative_list = mots_depuis_fichier("data/negative.txt")
         self.positive_list = mots_depuis_fichier("data/positive.txt")
-        print("BONJOUR")
-        print(self.negative_list)
-        print(self.positive_list)
-
 
     def analyser_tweets(self, liste_test_file):
         compteur_positive = 0
         compteur_negative = 0
         liste_test_analyse = []
         for tweet in liste_test_file:
+            print("caca")
+            print(tweet)
+            print(tweet[5])
             for mot in tweet[5].split():
+                print(mot)
                 if mot in self.positive_list:
                     compteur_positive += 1
                 elif mot in self.negative_list:
                     compteur_negative += 1
             if compteur_positive > compteur_negative:
-                liste_test_analyse.append([4] + tweet[1:])
+                liste_test_analyse.append(["4"] + tweet[1:])
             elif compteur_negative > compteur_positive:
-                liste_test_analyse.append([0] + tweet[1:])
+                liste_test_analyse.append(["0"] + tweet[1:])
             else:
-                liste_test_analyse.append([2] + tweet[1:])
+                liste_test_analyse.append(["2"] + tweet[1:])
             compteur_positive = 0
             compteur_negative = 0
         return liste_test_analyse
