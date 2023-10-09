98f0b45dac8a86b04c909f6ccfe18a3c3249c7dd
Samuel Dubuisson <samuel.dubuisson.etu@a12p5.fil.univ-lille.fr>
2023-10-09 13:49:42 +0200

    tweet in readme

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 
 # PJE-C : Analyse de sentiments sur Twitter
 
+## Structure d'un Tweet
+
+| Index | Part |
+|-------|-----------|
+| 0     | Class          |
+| 1     | Id          |
+| 2     | Date          |
+| 3     | ?          |
+| 4     | Author          |
+| 5     | Content          |
+
+## KNN 
+
+Réalisée : 
+- Distance de base
+- Distance de Jaccard
+- Détermination des voisins
+- Possibilité d'ajouter une fonction de pondération
+
--- sample/algorithms/KNN.py
+++ sample/algorithms/KNN.py
@@ -2,13 +2,37 @@ from Algorithm import Algorithm
 from collections import OrderedDict
 
 class KNN(Algorithm):
+
+    # t1 = "bla bla blabla" t2 = "blu bla blabla"
+    # private
+    def distance(t1, t2):
+        t1_list = t1.split()
+        t2_list = t2.split()
+        total_words_number = len(t1_list) + len(t2_list)
+        common_words_number = 0
+        history = set()
+        for word in t1_list:
+            if (word in t2_list) and (word not in history):
+                common_words_number += 1
+            history.add(word)    
+        return ((total_words_number - common_words_number) / total_words_number)
+    
+    def jaccard_distance(t1, t2):
+        t1_set = set(t1)
+        t2_set = set(t2)
+        intersection = len(list(t1_set.intersection(t2)))
+        union = (len(t1_set) + len(t2_set)) - intersection
+        return 1 - float(intersection) / union
+    
+
     def __init__(self):
         pass
     
     # sans réutilisation des tests déjà labélisés
     # sans pondération de la distance
     # TODO : fonction de pondération 
-    def analyser_tweets(self, test_list, learning_list, neighbours_number):
+    # method neighbour => prend en paramètre un neighbor, et renvoie sa classe multipliée par une pondération (par défaut 1). On peut imaginer multiplier par sa distance par exemple.
+    def analyser_tweets(self, test_list, learning_list, neighbours_number, ponderation_fonction = lambda neighbour : neighbour[0]):
         res = test_list.copy()
         neighbours = OrderedDict() # polarité : distance
         for tweet in test_list:
@@ -21,28 +45,13 @@ class KNN(Algorithm):
                         neighbours[labeled_data[0]] = distance
                 else :
                     neighbours[labeled_data[0]] = distance
-            tweet[0] = max(neighbours.keys(), lambda neighbour: neighbour[0])
+            tweet[0] = max(neighbours.keys(), lambda neighbour: ponderation_fonction(neighbour))
         return res
-
-    # t1 = "bla bla blabla" t2 = "blu bla blabla"
-    # private
-    def distance(self, t1, t2):
-        t1_list = t1.split()
-        t2_list = t2.split()
-        total_words_number = len(t1_list) + len(t2_list)
-        common_words_number = 0
-        history = set()
-        for word in t1_list:
-            if (word in t2_list) and (word not in history):
-                common_words_number += 1
-            history.add(word)    
-        return ((total_words_number - common_words_number) / total_words_number)
 
     
 
 if __name__ == "__main__":
-    knn = KNN()
-    print(knn.distance("bla bla test", "bla culk"))
-    print(knn.distance("bkuvlab grub", "blab grib"))
+    print(KNN.distance("bla bla test", "bla culk"), KNN.jaccard_distance("bla bla test", "bla culk"))
+    print(KNN.distance("bkuvlab grub", "blab grib"), KNN.jaccard_distance("bkuvlab grub", "blab grib"))
 
 
